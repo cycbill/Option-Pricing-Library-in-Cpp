11c5a4a0dae548490f1725d3015c3e15e5ed7001
cycbill <cuiyechang1991@gmail.com>
2019-08-08 18:25:28 -0400

    tridiagonal matrix algorithm

--- tridiagonal_matrix_algorithm/main.cpp
+++ tridiagonal_matrix_algorithm/main.cpp
@@ -4,13 +4,12 @@
 #include "tridiagonal_matrix_algorithm.h"
 
 int main() {
-	int N = 10;
-	std::vector<double> A(N, 1.0), B(N, 1.0), C(N, 1.0), D(N, 3.0), X(N, 0.0);
+	int N = 6;
+	std::vector<double> A(N, 1.0), B(N, 2.0), C(N, 1.0), X(N, 0.0);
+	std::vector<double> D {4.0, 8.0, 12.0, 16.0, 21.0, 19.0};
 
 	A[0] = 0.0;
 	C[N - 1] = 0.0;
-	D[0] = 2.0;
-	D[N - 1] = 2.0;
 
 	tridiag_mtx_algo(X.size(), A, B, C, D, X);
 
