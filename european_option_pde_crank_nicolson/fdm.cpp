35f90f8db6c5da9bace0a6db95d546cf945c2cff
cycbill <cuiyechang1991@gmail.com>
2019-08-09 09:12:59 -0400

    aa

---
+++ european_option_pde_crank_nicolson/fdm.cpp
@@ -0,0 +1,15 @@
+#include "fdm.h"
+
+FDMCrankNicolson::FDMCrankNicolson(double _x_dom, unsigned long _J,
+	double _t_dom, unsigned long _N,
+	ConvectionDiffusionPDE* _pde)
+	: FDMBase(_x_dom, _J, _t_dom, _N, _Deref_pre_z_)
+{
+	calculate_step_size();
+	set_initial_conditions();
+}
+
+void FDMCrankNicolson::calculate_step_size() {
+	dx = x_dom / static_cast<double>(J - 1);
+	dt = t_dom / static_cast<double>(N - 1);
+}
---
+++ european_option_pde_crank_nicolson/fdm.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../european_option_pde_euler/pde.h"
+#include "../european_option_pde_euler/fdm.h"
+
+class FDMCrankNicolson : public FDMBase
+{
+protected:
+	void calculate_step_size();
+	void set_initial_conditions();
+	void calculate_boundary_conditions();
+	void calculate_inner_domain();
+
+public:
+	FDMCrankNicolson(double _x_dom, unsigned long _J,
+					 double _t_dom, unsigned long _N,
+					 ConvectionDiffusionPDE* _pde);
+	
+	void step_march();
+};
+
