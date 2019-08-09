c3ebe9432c1cf19f3406bec2e12923cefa70d390
cycbill <cuiyechang1991@gmail.com>
2019-08-09 17:37:34 -0400

    AAAA

--- european_option_pde_crank_nicolson/fdm.cpp
+++ european_option_pde_crank_nicolson/fdm.cpp
@@ -12,4 +12,71 @@ FDMCrankNicolson::FDMCrankNicolson(double _x_dom, unsigned long _J,
 void FDMCrankNicolson::calculate_step_size() {
 	dx = x_dom / static_cast<double>(J - 1);
 	dt = t_dom / static_cast<double>(N - 1);
-} a
+}
+
+void FDMCrankNicolson::set_initial_conditions() {
+	// Spatial settings
+	double cur_spot = 0.0;
+
+	old_result.resize(J, 0.0);
+	new_result.resize(J, 0.0);
+	x_values.resize(J, 0.0);
+
+	for (unsigned long j = 0; j < J; j++) {
+		cur_spot = static_cast<double>(j) * dx;
+		old_result[j] = pde->init_cond(cur_spot);
+		x_values[j] = cur_spot;
+	}
+
+	// Temporal settings
+	prev_t = 0.0;
+	cur_t = 0.0;
+}
+
+void FDMCrankNicolson::calculate_boundary_conditions() {
+	new_result[0] = pde->boundary_left(prev_t, x_values[0]);
+	new_result[J - 1] = pde->boundary_right(prev_t, x_values[J - 1]);
+}
+
+void FDMCrankNicolson::calculate_inner_domain() {
+
+	std::vector<double> A(J, 0.0), B(J, 0.0), C(J, 0.0), D(J, 0.0), X(J, 0.0);
+
+	// Only use inner result indices (1 to J-2)
+	for (unsigned long j = 1; j < J - 1; j++) {
+		// Temporary variables and throughout
+		double dt_sig = dt * (pde->diff_coeff(prev_t, x_values[j]));
+		double dt_sig_2 = dt * dx * 0.5 * (pde->conv_coeff(prev_t, x_values[j]));
+
+		// Differencing coefficients (see \alpha, \beta and \gamma in text)
+		alpha = dt_sig - dt_sig_2;
+		beta = dx * dx - (2.0 * dt_sig) + (dt * dx * dx * (pde->zero_coeff(prev_t, x_values[j])));
+		gamma = dt_sig + dt_sig_2;
+
+		// Update inner values of spatial discretisation grid (Explicit Euler)
+		new_result[j] = ((alpha * old_result[j - 1]) +
+			(beta * old_result[j]) +
+			(gamma * old_result[j + 1])) / (dx*dx) -
+			(dt*(pde->source_coeff(prev_t, x_values[j])));
+	}
+
+
+}
+
+void FDMCrankNicolson::step_march() {
+	std::ofstream fdm_out("fdm.csv");
+
+	while (cur_t < t_dom) {
+		cur_t = prev_t + dt;
+		calculate_boundary_conditions();
+		calculate_inner_domain();
+		for (unsigned long j = 0; j < J; j++) {
+			fdm_out << x_values[j] << " " << prev_t << " " << new_result[j] << std::endl;
+		}
+
+		old_result = new_result;
+		prev_t = cur_t;
+	}
+
+	fdm_out.close();
+}
--- tridiagonal_matrix_algorithm/tridiagonal_matrix_algorithm.h
+++ tridiagonal_matrix_algorithm/tridiagonal_matrix_algorithm.h
@@ -8,24 +8,6 @@ void tridiag_mtx_algo(size_t N,
 	std::vector<double>& C,
 	std::vector<double> D,
 	std::vector<double>& X)
-	/*
-	{
-		double W;
-
-		for (int i = 1; i < N; i++) {
-			W = A[i] / B[i - 1];
-			B[i] = B[i] - W * C[i - 1];
-			D[i] = D[i] - W * D[i - 1];
-		}
-
-		X[N - 1] = D[N - 1] / B[N - 1];
-		for (int i = N - 2; i >= 0; i--) {
-			X[i] = (D[i] - C[i] * X[i + 1]) / B[i];
-		}
-
-		return;
-	}
-	*/
 {
 	std::vector<double> C_prime(N, 0.0), D_prime(N, 0.0);
 
