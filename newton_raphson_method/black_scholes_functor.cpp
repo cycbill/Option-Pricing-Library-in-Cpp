46b17c597f2eb270e5583bf7568f07249345134b
cycbill <cuiyechang1991@gmail.com>
2019-08-25 12:37:45 -0400

    newton raphson method

---
+++ newton_raphson_method/black_scholes_functor.cpp
@@ -0,0 +1,17 @@
+#include "black_scholes_functor.h"
+
+#include "../black_scholes_vanilla/black_scholes_formula.h"
+#include "../black_scholes_vanilla/standard_normal_functions.h"
+
+
+BlackScholes::BlackScholes(int _callput, double _S, double _K,
+	double _rd, double _rf, double _te, double _td)
+	: callput(_callput), S(_S), K(_K), rd(_rd), rf(_rf), te(_te), td(_td) {}
+
+double BlackScholes::option_price(double vol) const {
+	return bsvanilla_premium(callput, S, K, vol, rd, rf, te, td);
+}
+
+double BlackScholes::option_vega(double vol) const {
+	return bsvanilla_vega(S, K, vol, rd, rf, te, td);
+}
--- newton_raphson_method/main.cpp
+++ newton_raphson_method/main.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
-
-#include "interval_bisection.h"
 #include "black_scholes_functor.h"
+#include "newton_raphson.h"
 
 int main()
 {
+	// First we create the parameter list
 	int callput;
 	double S, K, rd, rf, te, td;
 
 	callput = 1;
-	S = 7.7610;	
+	S = 7.7610;
 	K = 7.7481;
 	rd = 0.001444440118;
 	rf = 0.001549120310;
 	te = 0.079452054795;
 	td = 0.084931506849;
 
-	// Create the Black-Scholes Formula functor
+	// Create the Black-Scholes option functor
 	BlackScholes BSVan(callput, S, K, rd, rf, te, td);
 
 	// test
-	double prem = BSVan.operator()(0.10);
+	double prem = BSVan.option_price(0.10);
 	std::cout << "Premium with vol = 0.10: " << prem << std::endl;		//0.0937515
 
-	// Interval Bisection parameters
-	double low_vol = 0.05;
-	double high_vol = 0.35;
-	double episilon = 0.000001;
+	// Newton Raphson parameters
+	double init = 0.05; // Our guess impl. vol of 5%
+	double epsilon = 0.000001;
 
 	// Calculate the implied volatility
-	double imp_vol = interval_bisection(prem, low_vol, high_vol, episilon, BSVan);
+	double imp_vol = newton_raphson<BlackScholes,
+		&BlackScholes::option_price,
+		&BlackScholes::option_vega>
+		(prem, init, epsilon, BSVan);
+
+	// Output the values
 	std::cout << "Implied Vol: " << imp_vol << std::endl;
 
 	std::cin.get();
